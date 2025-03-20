b24d0e971aec4687ff6988d6a062bf2a0fb6801e
Jacuzzi <jheklayag@gmail.com>
2025-03-20 18:10:46 +0800

    Fixed pseudocode and added user input

--- formatted_number_0_to_1000.py
+++ formatted_number_0_to_1000.py
@@ -1,4 +1,6 @@
-# Allow the user to input a number
+# Allow the user to input a number while in a loop
+while True:
+    i = int(input("Enter a number: "))
 # Set a condition that only allows valid numbers
 # Format the input into 6 digits
 # Print result
