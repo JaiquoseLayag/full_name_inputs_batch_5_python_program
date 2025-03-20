f267c897db9dcc7bb2eccb25dc72bd8016d21b54
Jacuzzi <jheklayag@gmail.com>
2025-03-20 17:56:42 +0800

    .strip() has been added and full name is printed

--- full_name_without_spaces.py
+++ full_name_without_spaces.py
@@ -1,4 +1,5 @@
 # Allow the user to input their full name wtih spaces
 # Use the .strip() function to remove spaces
-full_name = input("Enter your full name: ")
+full_name = input("Enter your full name: ").strip()
 # Print result
+print(full_name)
