58dd7221ffbce04d0a29f66e18b7f7a5a0f891ae
Jacuzzi <jheklayag@gmail.com>
2025-03-20 19:02:18 +0800

    Several spaces deleted and result has been printed

--- full_name_in_lowercase_format.py
+++ full_name_in_lowercase_format.py
@@ -1,4 +1,5 @@
 # Allow the user to input their full name
- # Capitalize the input with .lower() function
- full_name = input("Enter your full name: ").lower()
- # Print result
+# Capitalize the input with .lower() function
+full_name = input("Enter your full name: ").lower()
+# Print result
+print(full_name)
