50f1811de7301b1aa7d9eaf6c97384d7be3d9359
Jacuzzi <jheklayag@gmail.com>
2025-03-20 18:11:48 +0800

    Added a condition for valid inputs

--- formatted_number_0_to_1000.py
+++ formatted_number_0_to_1000.py
@@ -2,5 +2,9 @@
 while True:
     i = int(input("Enter a number: "))
 # Set a condition that only allows valid numbers
+    if 0 <= i <= 1000:
+        break  
+    else:
+        print("Please enter a valid number.")
 # Format the input into 6 digits
 # Print result
