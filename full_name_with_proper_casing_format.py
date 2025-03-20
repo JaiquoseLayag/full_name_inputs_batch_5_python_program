897ace7cfc88e7630cc9079e5bf344c3131560b7
Jacuzzi <jheklayag@gmail.com>
2025-03-20 19:12:54 +0800

    Input has been formatted to proper casing with .title() function

--- full_name_with_proper_casing_format.py
+++ full_name_with_proper_casing_format.py
@@ -1,4 +1,4 @@
 # Allow the user to input their full name
 # Format the input using .title()
-full_name = input("Enter you full name: ")
+full_name = input("Enter you full name: ").title()
 # Print result
