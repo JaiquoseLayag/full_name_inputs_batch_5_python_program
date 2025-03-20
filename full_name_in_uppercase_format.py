c4ffdff413190001c2e4f923d042238b53fd3ff1
Jacuzzi <jheklayag@gmail.com>
2025-03-20 18:23:35 +0800

    Input has been set to uppercase by .upper() function

--- full_name_in_uppercase_format.py
+++ full_name_in_uppercase_format.py
@@ -1,4 +1,4 @@
 # Allow the user to input their full name
 # Capitalize the input with .upper() function
-full_name = input("Enter youfull name: ")
+full_name = input("Enter youfull name: ").upper()
 # Print result
