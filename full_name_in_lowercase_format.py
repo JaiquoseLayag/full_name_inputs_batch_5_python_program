5d2260ab2bc84d8dc51ef6bcc126bae429df4e50
Jacuzzi <jheklayag@gmail.com>
2025-03-20 19:00:49 +0800

    Input has been formatted to lowercase with .lower() function

--- full_name_in_lowercase_format.py
+++ full_name_in_lowercase_format.py
@@ -1,4 +1,4 @@
 # Allow the user to input their full name
  # Capitalize the input with .lower() function
- full_name = input("Enter your full name: ")
+ full_name = input("Enter your full name: ").lower()
  # Print result
