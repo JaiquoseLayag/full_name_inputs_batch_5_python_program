378de7892265ab544323ad354ef7978d983a96a5
Jacuzzi <jheklayag@gmail.com>
2025-03-21 13:25:03 +0800

    .title() formats the input in Pascal Case and .replace() removes spaces

--- format_full_name_in_pascal_case.py
+++ format_full_name_in_pascal_case.py
@@ -1,5 +1,5 @@
 # Allow the user to input their full name
 # Use .title() to format the input in correct casing
 # Use .replace() to remove spaces
-full_name = input("Enter your full name: ")
+full_name = input("Enter your full name: ").title().replace(" ", "")
 # Print result
