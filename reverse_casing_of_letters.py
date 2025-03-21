7f65b7654c31a7b4e478715e61f42dac34387044
Jacuzzi <jheklayag@gmail.com>
2025-03-21 12:55:53 +0800

    Fixed a mistake

--- reverse_casing_of_letters.py
+++ reverse_casing_of_letters.py
@@ -1,5 +1,5 @@
 # Allow the user to input their full name
 # Reverse the casing of each letter by using .swapcase()
-full_name = input("Enter your full_namre: ").swapcase()
+full_name = input("Enter your full_name: ").swapcase()
 # Print result
 print(full_name)
