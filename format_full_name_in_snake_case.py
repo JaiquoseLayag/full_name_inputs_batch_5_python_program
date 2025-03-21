a40a7af2655c7f238310cd4ce6f90570fae5e989
Jacuzzi <jheklayag@gmail.com>
2025-03-21 13:32:28 +0800

    User input for full name has been added

--- format_full_name_in_snake_case.py
+++ format_full_name_in_snake_case.py
@@ -1,4 +1,5 @@
 # Allow the user to input their full name
 # Use .lower() to format the input in lowercase format
 # Use .replace() to replace spaces with underscores (_)
+full_name = input("Enetr your full name: ")
 # Print result
