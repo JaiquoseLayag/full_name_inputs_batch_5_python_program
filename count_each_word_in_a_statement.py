8de8e9db9aeaa040ca6159136d805aa7278ce08d
Jacuzzi <jheklayag@gmail.com>
2025-03-21 13:03:58 +0800

    Pseudocode

---
+++ count_each_word_in_a_statement.py
@@ -0,0 +1,4 @@
+# Allow the user to input a complete statement
+# Use the .split() method to split the input into a list of words
+# Use len() to count the number of words in the list
+# Print the result
