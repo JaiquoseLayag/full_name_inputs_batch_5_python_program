7af650bab08d7d9635e21346af1179de818316c2
Jacuzzi <jheklayag@gmail.com>
2025-03-21 13:05:55 +0800

    len() function has been used to count the words

--- count_each_word_in_a_statement.py
+++ count_each_word_in_a_statement.py
@@ -2,4 +2,5 @@
 # Use the .split() method to split the input into a list of words
 staement = input("Enter a statement: ").split()
 # Use len() to count the number of words in the list
+count = len(statement)
 # Print the result
