ee59bb14265f1a7f548115783d3ad5346f3f7c63
Jacuzzi <jheklayag@gmail.com>
2025-03-21 13:41:02 +0800

    Fixed the code to only read words and not numbers

--- count_each_word_in_a_statement.py
+++ count_each_word_in_a_statement.py
@@ -1,7 +1,10 @@
 # Allow the user to input a complete statement
 # Use the .split() method to split the input into a list of words
 staement = input("Enter a statement: ").split()
-# Use len() to count the number of words in the list
-count = len(statement)
+# Use .isalpha() in a loop to count words only and not numbers
+count = 0
+for word in statement:
+    if word.isalpha()
+        count += 1
 # Print the result
 print(count)
