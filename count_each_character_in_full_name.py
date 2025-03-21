76cd3a5b8fe961a014080dde53614c8fb82aa0bf
Jacuzzi <jheklayag@gmail.com>
2025-03-21 13:15:37 +0800

    User input for full name has been added

--- count_each_character_in_full_name.py
+++ count_each_character_in_full_name.py
@@ -1,4 +1,5 @@
 # Allow the suer to input their full name
+full_name = input("Enter your full name: ")
 # Use len() function to count each letter
 # Use .replace() to remove spaces in the input
 # Print result
