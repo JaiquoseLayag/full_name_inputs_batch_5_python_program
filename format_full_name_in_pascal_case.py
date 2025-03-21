5c9629a2940db67a665e93042b398174ebf300aa
Jacuzzi <jheklayag@gmail.com>
2025-03-21 13:24:00 +0800

    User input for full name has been addeed

--- format_full_name_in_pascal_case.py
+++ format_full_name_in_pascal_case.py
@@ -1,4 +1,5 @@
 # Allow the user to input their full name
 # Use .title() to format the input in correct casing
 # Use .replace() to remove spaces
+full_name = input("Enter your full name: ")
 # Print result
