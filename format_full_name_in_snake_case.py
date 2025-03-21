b30787f226e669d7598da8f96d09bb9314933dfc
Jacuzzi <jheklayag@gmail.com>
2025-03-21 13:33:41 +0800

    .lower() formats the input in lowercase and .replace() replaces spaces with underscores

--- format_full_name_in_snake_case.py
+++ format_full_name_in_snake_case.py
@@ -1,5 +1,5 @@
 # Allow the user to input their full name
 # Use .lower() to format the input in lowercase format
 # Use .replace() to replace spaces with underscores (_)
-full_name = input("Enetr your full name: ")
+full_name = input("Enetr your full name: ").lower().replace(" ", "_")
 # Print result
