8815f3f38501300123ce81bc6bb74f6a87400dfe
Jacuzzi <jheklayag@gmail.com>
2025-03-21 12:54:33 +0800

    Casing has been reverse by using .swapcase() function

--- reverse_casing_of_letters.py
+++ reverse_casing_of_letters.py
@@ -1,4 +1,4 @@
 # Allow the user to input their full name
 # Reverse the casing of each letter by using .swapcase()
-full_name = input("Enter your full_namre: ")
+full_name = input("Enter your full_namre: ").swapcase()
 # Print result
