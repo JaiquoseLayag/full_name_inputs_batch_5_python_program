569b0be7c3bafd7d59414793fa1bf19ab74669f8
Jacuzzi <jheklayag@gmail.com>
2025-03-21 13:05:19 +0800

    User input for a statement has been added and .split() function has been used

--- count_each_word_in_a_statement.py
+++ count_each_word_in_a_statement.py
@@ -1,4 +1,5 @@
 # Allow the user to input a complete statement
 # Use the .split() method to split the input into a list of words
+staement = input("Enter a statement: ").split()
 # Use len() to count the number of words in the list
 # Print the result
